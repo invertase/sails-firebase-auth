59edaac9f8af4e1829e53680974a94ffb16573cd
Salakar <mike.diarmid@gmail.com>
2017-11-14 09:40:44 +0000

    misc formatting

--- api/policies/firebase.js
+++ api/policies/firebase.js
@@ -2,7 +2,7 @@ const firebase = require('firebase-admin');
 
 firebase.initializeApp({
   credential: firebase.credential.cert(require('../../firebase-service-account.json')),
-  databaseURL: ''
+  databaseURL: '' // TODO paste your URL HERE
 });
 
 module.exports = function (req, res, next) {
--- get-token-test.js
+++ get-token-test.js
@@ -1,20 +1,15 @@
 const firebase = require('firebase');
 
 firebase.initializeApp({
-  apiKey: "AIzaSyBmmUna_PI_2r4YmLw4_DzC8ONaOQiIttY",
-  authDomain: "affiliate-f3897.firebaseapp.com",
-  databaseURL: "https://affiliate-f3897.firebaseio.com",
-  projectId: "affiliate-f3897",
-  storageBucket: "affiliate-f3897.appspot.com",
-  messagingSenderId: "738776502639"
+  // TODO paste web sdk credentials here
 });
 
-firebase.auth().signInAnonymously().then(()=> {
-  firebase.auth().currentUser.getIdToken(/* forceRefresh */ true).then(function(idToken) {
+firebase.auth().signInAnonymously().then(() => {
+  firebase.auth().currentUser.getIdToken(/* forceRefresh */ true).then(function (idToken) {
     console.log(idToken);
     debugger;
     // ...
-  }).catch(function(error) {
+  }).catch(function (error) {
     console.error(error);
   });
 });
